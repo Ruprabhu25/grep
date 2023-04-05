c0110295c6ff2e34a7e80bab76280df3f0dfb4b8
Ruprabhu25 <ruprabhu25@gmail.com>
2023-04-04 18:23:00 -0700

    fixed linked list and added command line parsing, need to add parse file now

--- example.c
+++ example.c
@@ -16,38 +16,59 @@ struct node {
 struct node *head = NULL;
 struct node *current = NULL;
 
-void create_list(char* input_string) {
-	//printf("Here\n");
-    char *token;
-    //printf("Here\n");
-    /*get the first token */
-    token = strtok(input_string, " ");
-	//printf("Here\n");
-    /* walk through other tokens */
-	while( token != NULL ) {
-    	//printf( " %s\n", token );
-		struct node *newNode = malloc(sizeof(struct node));
-		newNode->val = token;
-		newNode->next = NULL;
+void add_node(char* pattern) {
+	struct node *newNode = malloc(sizeof(struct node));
+	newNode->val = pattern;
+	newNode->next = NULL;
 
-		//if head is NULL, it is an empty list
-		if(head == NULL) {
-			head = newNode;
-			current = head;
-		}
-		//Otherwise, find the last node and add the newNode
-		else
-		{
-			//add the newNode at the end of the linked list
-			current->next = newNode;
-			current = current->next;
-    	}
-		token = strtok(NULL, " ");
+	//if head is NULL, it is an empty list
+	if(head == NULL) {
+		head = newNode;
+		current = head;
 	}
-	
+	//Otherwise, find the last node and add the newNode
+	else
+	{
+		//add the newNode at the end of the linked list
+		current->next = newNode;
+		current = current->next;
+	}
 }
 
-int main(void)
+// determine which commands are being used
+void iterate_commands(char *argv[]) {
+	int is_numbered = 0;
+	int is_colored = 0;
+	int help = 0;
+	int len = sizeof(argv) / sizeof(char*);
+	int i = 1;
+	while (i < len) {
+		if (strcomp(argv[i]) == "-h") {
+			printf("Usage: ./sgrep [-n] [-c] [-p PATTERN]... FILE...\n");
+			return;
+		}
+		else if (strcomp(argv[i]) == "-n") {
+			is_numbered = 1;
+		}
+		else if (strcomp(argv[i]) == "-c") {
+			is_colored = 1;
+		}
+		else if (strcomp(argv[i]) == "-p") {
+			i = i + 1;
+			add_node(argv[i]);
+		}
+		i = i + 1;
+	}
+	char filename[] = argv[len-1];
+	parse_file(filename, is_numbered, is_colored);
+	return;
+}
+
+void parse_file() {
+	return;
+}
+
+int main(int argc, char *argv[])
 {
 	char str[] = "I love ECS150!";
 	char *love = "love";
